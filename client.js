87e043596d9a929e5727e353eb5f58ded1e0ca7e
Panashe Jere <panashejere2005@gmail.com>
2025-04-26 09:44:05 +0100

    another commit

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 import WebSocket from 'ws'
 
-const ws = new WebSocket('ws://127.0.0.1:8000/ws')
+const ws = new WebSocket('ws://sign-language-generator.onrender.com:8000')
 
 ws.on('open', () => {
     console.log('Connection started')
 
     ws.send(JSON.stringify({'msg': 'produce', 'data': 'Hey my name is Panashe Jere'}))
 })
-ws.on('error', () => console.log(`Connection disconnected: ${e.message}`))
+ws.on('error', (e) => console.log(`Connection disconnected: ${e.message}`))
 
 ws.on('message', m => {
     console.log(m.toString())
--- server.py
+++ server.py
@@ -16,48 +16,28 @@ def parse_client_msg (data):
 app = FastAPI()
 app.mount('/videos', StaticFiles(directory='exports'), name='static')
 
-@app.websocket('/ws')
-async def sl (websocket: WebSocket):
-    await websocket.accept()
+@app.get('create/{text}')
+async def sl (text):
+    id = genRanStr(10)
 
-    while True:
-        data = await websocket.receive_text()
-        data = json.loads(data)
+    #Preprocess
+    sentence = scripts.preprocess(text)
+
+    #Get words
+    words = scripts.get_words(sentence)
+
+    #Create images
+    for w, i in zip(words, range(len(words))):
+        img = scripts.create_img_word(w, 10)   
+        os.makedirs(f'builds/{id}', exist_ok=True)
+        img.save(f'./builds/{id}/{i}.jpg')
 
 
-        if data['msg'] == 'produce':
-            text = data['data'] 
+    #Video generation
+    scripts.generate_video(id)
 
-            id = genRanStr(10)
-            await websocket.send_text(json.dumps({'msg': 'Video ID', 'data': id}))
+    #Remove build folder
+    scripts.del_folder(id)
 
-            #Preprocess
-            sentence = scripts.preprocess(text)
-
-            #Get words
-            words = scripts.get_words(sentence)
-
-            #Create images
-            for w, i in zip(words, range(len(words))):
-                img = scripts.create_img_word(w, 10)   
-                os.makedirs(f'builds/{id}', exist_ok=True)
-                img.save(f'./builds/{id}/{i}.jpg')
-
-                #Send progress
-                await websocket.send_text(json.dumps({'msg': 'Image generation', 'data': f'{i + 1}/{len(words)}'}))
-
-            #Video generation
-            scripts.generate_video(id)
-
-            #Remove build folder
-            scripts.del_folder(id)
-
-            #Send completion
-            await websocket.send_text(json.dumps({'msg': 'Done! File path in {data}', 'data': id}))
-
-            try:
-                await websocket.close(1000, 'Terminated')
-                break
-
-            except WebSocketDisconnect:
-                pass
+    #Send completion
+    return {'message': 'Complete', 'data': NotImplementedError}
